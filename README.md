2991122a1c31d5082799728c0485f74a3b24c81e
albert.liao <alb423@gmail.com>
2018-03-04 20:19:52 +0800

    support menu options when "make menuconfig"

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 
+
 README
 ===========================
 A Test package for build root test
@@ -16,11 +17,14 @@ A Test package for build root test
 * wget https://buildroot.org/downloads/buildroot-2017.11.tar.gz
 * tar xvf buildroot-2017.11.tar.gz
 * cd buildroot-2017.11\package\
+* vi Config.in, \
+    add below configuration, so that you can enable/disable mytest package by menuconfig\
+    source "package/mytest/Config.in"
 * mkdir mytest
-* cp mytest
+* cd mytest
 * cp ~/buildroot_test/mytest.mk .
 * cp ~/buildroot_test/Config.in .
-* 
+
 
 ## create source code and assign correct path
 * cd ~
@@ -29,7 +33,7 @@ A Test package for build root test
 * cp ~/buildroot_test/Makefile .
 * cp ~/buildroot_test/mytest.c .
 * vi buildroot-2017.11\package\ , change below setting to correct path
-  MYTEST_OVERRIDE_SRCDIR = /home/user_name/code/mytest/
+   MYTEST_OVERRIDE_SRCDIR = /home/user_name/code/mytest/
 
 ## rebuild package
 * cp ~/buildroot-2017.11
@@ -38,3 +42,4 @@ A Test package for build root test
 
 ## Reference:
 * https://buildroot.org/downloads/manual/manual.html
+* http://albert-oma.blogspot.com/2018/03/embedded-buildroot-qemuarm.html
--- mytest.mk
+++ mytest.mk
@@ -5,22 +5,29 @@
 ################################################################################
 
 MYTEST_VERSION = 1.0
+MYTESTSOURCE = ""
+MYTEST_SITE = ""
 MYTEST_OVERRIDE_SRCDIR = /home/albert/code/mytest/
+
 
 MYTEST_INSTALL_STAGING = YES
 MYTEST_INSTALL_TARGET = YES
 
-#MYTEST_CONF_OPTS += --without-python-support
-#MYTEST_DEPENDENCIES += $(if $(BR2_PACKAGE_PYTHON),python,)
+MYTEST_CONF_OPTS += --without-python-support
 
 define MYTEST_REMOVE_DOC
 	$(RM) -fr $(TARGET_DIR)/usr/share/mytest/doc \
 		$(TARGET_DIR)/usr/share/man/man3/mytest*
 endef
 
+# Dependence
+MYTEST_CONF_OPTS += --with$(if $(BR2_PACKAGE_PYTHON),,out)-python-support
+MYTEST_DEPENDENCIES += $(if $(BR2_PACKAGE_PYTHON),python,)
+
 ifneq ($(BR2_MYTEST_INSTALL_DATA),y)
 	MYTEST_POST_INSTALL_TARGET_HOOKS += MYTEST_REMOVE_DATA
 endif
+
 
 define MYTEST_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) all
@@ -37,8 +44,8 @@ define MYTEST_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/mytest $(TARGET_DIR)/usr/bin
 endef
 
-define MYTEST_PERMISSIONS
-	/bin/mytest f 0755 mytest libmytest - - - - -
-endef
+#define MYTEST_PERMISSIONS
+#	/bin/mytest f 0755 mytest  - - - - -
+#endef
 
 $(eval $(generic-package))
